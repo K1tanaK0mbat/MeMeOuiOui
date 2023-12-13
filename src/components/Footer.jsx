dc08a126e556110ccb4170abf50464ab9c3187d5
K1tanaK0mbat <kkeayouth@gmail.com>
2023-12-13 13:05:54 -0600

    app working

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,10 +3,10 @@ import React from 'react';
 
 export default function Footer () {
     return (
-<div>
-    <a href="https://github.com/K1tanaK0mbat">Github</a>
-    <a href="www.linkedin.com/in/kitana-woodley-kearney-36a15b228">Linkdin</a>
-</div>
+<ul id="footer">
+    <li><a href="https://github.com/K1tanaK0mbat">Github</a></li>
+    <li><a href="www.linkedin.com/in/kitana-woodley-kearney-36a15b228">Linkdin</a></li>
+</ul>
 
     );
 }
