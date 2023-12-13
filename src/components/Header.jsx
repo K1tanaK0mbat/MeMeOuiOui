a012bc87563cb6a9dfe097f12cc51a69d3172086
K1tanaK0mbat <kkeayouth@gmail.com>
2023-12-13 12:29:07 -0600

    added styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,3 +1,4 @@
+import React from 'react';
 
 function NavHeader({ currentPage, handlePageChange }) {
     return (
@@ -7,25 +8,25 @@ function NavHeader({ currentPage, handlePageChange }) {
         <li>
           <a href="#AboutMe"
             onClick={() => handlePageChange('About Me')}
-            //className={currentPage === 'About Me' ?  : }
+            className={currentPage === 'About Me' ? 'active' : ''}
           > About Me </a>
         </li>
         <li>
           <a href="#Portfolio"
             onClick={() => handlePageChange('Portfolio')}
-            //className={currentPage === 'Portfolio' ?  : }
+            className={currentPage === 'Portfolio' ? 'active' : ''}
           > Portfolio </a>
         </li>
         <li>
           <a href="#Resume"
             onClick={() => handlePageChange('Resume')}
-           // className={currentPage === 'Resume' ?  :}
+           className={currentPage === 'Resume' ? 'active' :''}
           > Resume </a>
         </li>
         <li>
           <a href="#contact"
             onClick={() => handlePageChange('Contact')}
-            //className={currentPage === 'Contact' ?  : }
+            className={currentPage === 'Contact' ? 'active' :'' }
           > Contact </a>
         </li>
       </ul>
