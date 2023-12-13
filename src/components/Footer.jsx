3d394f74dedbc0fb24131d8734a91e1a2439a86f
K1tanaK0mbat <kkeayouth@gmail.com>
2023-12-13 12:52:18 -0600

    made footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+
+import React from 'react';
+
+export default function Footer () {
+    return (
+<div>
+    <a href="https://github.com/K1tanaK0mbat">Github</a>
+    <a href="www.linkedin.com/in/kitana-woodley-kearney-36a15b228">Linkdin</a>
+</div>
+
+    );
+}
