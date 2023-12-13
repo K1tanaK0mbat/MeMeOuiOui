09976f54cb5babc3409b5c30360d404c8f7789e9
K1tanaK0mbat <kkeayouth@gmail.com>
2023-12-13 13:27:06 -0600

    pic

--- src/pages/Resume.jsx
+++ src/pages/Resume.jsx
@@ -5,7 +5,7 @@ export default function Resume ()
     return (
 <div>
 <h1>My Resume</h1>
-<a href="src\assets\_Dec23Update_ResumeKitana.html">My Resume</a>
+<a href="/assets/_Dec23Update_ResumeKitana.html">My Resume</a>
 
 <h2>Front End-Profencies</h2>
 <ul>
